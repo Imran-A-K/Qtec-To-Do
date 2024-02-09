aaafece7438b5883e101357df77f28b939273690
Imran-A-K <imran.k.professional@gmail.com>
2024-02-09 10:37:14 +0600

    Added task management functions

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,48 @@ import useLocalStorage from "./hooks/useLocalStorage";
 function App() {
   const [tasks, setTasks] = useLocalStorage("todo-tasks", []);
   const [addingTask, setAddingTask] = useState(false);
+  const [editedTask, setEditedTask] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
   const [previousFocusEl, setPreviousFocusEl] = useState(null);
 
   const addTask = (task) => {
     setTasks((prevState) => [...prevState, task]);
   };
+
+  const deleteTask = (id) => {
+    setTasks((prevState) => prevState.filter((t) => t.id !== id));
+  };
+
+  const toggleTask = (id) => {
+    setTasks((prevState) =>
+      prevState.map((t) => (t.id === id ? { ...t, checked: !t.checked } : t))
+    );
+  };
+
+  const updateTask = (task) => {
+    setTasks((prevState) =>
+      prevState.map((t) =>
+        t.id === task.id
+          ? { ...t, name: task.name, priority: task.priority }
+          : t
+      )
+    );
+    closeEditMode();
+  };
+
+  const closeEditMode = () => {
+    setIsEditing(false);
+    previousFocusEl.focus();
+  };
   const closeAddTask = () => {
     setAddingTask(false);
     previousFocusEl.focus();
+  };
+
+  const enterEditMode = (task) => {
+    setEditedTask(task);
+    setIsEditing(true);
+    setPreviousFocusEl(document.activeElement);
   };
   return (
     <div className="container">
